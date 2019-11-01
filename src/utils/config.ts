924b169c42c78fe7934051ecd3a97b7b1cf31b8e
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-01 14:08:03 -0600

    Refactor util for configuration
    * init() and load() now take a path as parameter
    * normalize() normalizes configurations correctly according to tests

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,50 +1,69 @@
-import * as fs from 'fs'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import * as yml from 'js-yaml'
 import { join } from 'path'
 
-import { IConfig, IPackage } from '../interfaces/config'
+import { IConfig, IConfigInitOptions, IUserConfig, IUserPackage, IUserTask } from '../interfaces/config'
+import { IFrameworkKind } from '../interfaces/framework'
 
 export const KO_CONFIG_FILENAME = 'ko.config.yml'
 export const KO_CONFIG_REPOSITORY_URL = 'https://www.github.com/prismify-co/ko-tasks/'
 
 export let KO_CONFIG_CURRENT_REPOSITORY_URL = KO_CONFIG_REPOSITORY_URL
 
-export function exists(path: string = process.cwd()): boolean {
-  return fs.existsSync(join(path, KO_CONFIG_FILENAME))
-}
-
-export function init(name: string, framework: string, version: string) {
-  if (exists()) {
+export function init(options: IConfigInitOptions) {
+  // Normalize the path
+  const path = (options.path || process.cwd()).replace(KO_CONFIG_FILENAME, '')
+  // Check if the file exist
+  if (existsSync(join(path, KO_CONFIG_FILENAME))) {
     return
   }
-
-  return fs.writeFileSync(join(process.cwd(), KO_CONFIG_FILENAME), yml.dump({
-    name,
-    framework: {
-      name: framework,
-      version
-    }
+  return writeFileSync(join(path, KO_CONFIG_FILENAME), yml.dump({
+    name: options.name,
+    framework: options.framework
   }, { flowLevel: 3 }), 'utf8')
 }
 
-export function load(): IConfig {
-  if (!exists()) throw new Error('Configuration file does not exist.')
-  return normalize(yml.safeLoad(fs.readFileSync(join(process.cwd(), KO_CONFIG_FILENAME), 'utf8')) as IConfig)
+export function load(path: string = process.cwd()): IConfig {
+  // Normalize the path
+  const configPath = path.replace(KO_CONFIG_FILENAME, '')
+  if (!existsSync(configPath)) throw new Error('Configuration file does not exist.')
+  return normalize(yml.safeLoad(readFileSync(join(configPath, KO_CONFIG_FILENAME), 'utf8')) as IUserConfig)
 }
 
-function normalize(config: IConfig): IConfig {
+export function normalize(config: IUserConfig): IConfig {
   // Set the app name
   let result: any = { name: config.name }
 
   // Normalize the framework if it's as string
   if (typeof config.framework === 'string') {
     result.framework = { name: config.framework, version: 'latest' }
-  } else result.framework = config.framework
+  } else {
+    const keys = Object.keys(config.framework)
+    if (keys.length > 0 && !config.framework.name && !config.framework.version) {
+      const key = keys.filter(key => key !== 'name' && key !== 'version')[0] as IFrameworkKind
+      const value = config.framework[key]
+      let version = 'latest'
+      if (typeof value === 'string') {
+        version = value
+      } else if (value.version) {
+        version = value.version
+      }
+
+      result.framework = {
+        name: key,
+        version
+      }
+    } else if (config.framework.name && config.framework.version) {
+      result.framework = config.framework
+    }
+  }
 
   // Normalize the repository's base url if it's a string
   if (typeof config.repository === 'string') {
-    result.repository = { url: config.repository }
-  } else result.repository = (config.repository || {})
+    result.repository = { url: config.repository || KO_CONFIG_REPOSITORY_URL }
+  } else result.repository = (config.repository || {
+    url: KO_CONFIG_REPOSITORY_URL
+  })
 
   KO_CONFIG_CURRENT_REPOSITORY_URL = result.repository.url || KO_CONFIG_REPOSITORY_URL
 
@@ -67,14 +86,16 @@ function normalize(config: IConfig): IConfig {
 
       // The package could be an object that
       // the name of the package to run is the key
-      if (!task.task.name) {
+      if (!task.task.name && !task.task.url && !task.task.version) {
         const key = Object
           .keys(task.task)
           .filter(k => k !== 'version' && k !== 'url')[0]
-        let value = (task.task as any)[key] as IPackage
-        let url = value.url || KO_CONFIG_CURRENT_REPOSITORY_URL
-        let version = value.version || 'latest'
-        return { name: task.name, task: { name: key, url, version } }
+        let value = (task.task as any)[key] as IUserPackage
+        let url = (value.url || KO_CONFIG_CURRENT_REPOSITORY_URL) as string
+        let version = (value.version || 'latest') as string
+        // Normalize versions
+        version = version.replace('v', '')
+        return { name: task.name, task: { name: key, url, version } } as IUserPackage
       }
 
       if (!task.task.name) {
@@ -89,9 +110,9 @@ function normalize(config: IConfig): IConfig {
         task.task.version = 'latest'
       }
 
-      return task
+      return task as IUserTask
     })
-  } else result.tasks = config.tasks
+  } else result.tasks = config.tasks || []
 
-  return result
+  return (result as IConfig)
 }
