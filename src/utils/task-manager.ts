4b3ed71476b802e304b6055d7c18ac764f3642e5
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-01 22:42:47 -0600

    Add task manager

--- src/utils/task-manager.ts
+++ src/utils/task-manager.ts
@@ -1,1 +1,36 @@
-export function manage() { }
+import { cli } from 'cli-ux'
+import { join } from 'path'
+import { cd, exec } from 'shelljs'
+
+import { IConfig } from '../interfaces/config'
+import { ITaskRunner } from '../interfaces/task-runner'
+
+export function manage(config: IConfig) {
+  if (Array.isArray(config.tasks)) {
+    config.tasks.forEach(async item => {
+      if (item.task.url !== config.repository.url) {
+        // TODO: Handle tasks that have a different url
+      }
+      // Save the current root directory
+      const rootDir = process.cwd()
+      // Create the path to node_modules
+      const nodeModules = join(rootDir, 'node_modules')
+      // Create the path to the task's directory
+      const taskDir = join(config.repository.name, item.task.name, config.framework.name)
+      // Create the full path
+      const path = join(nodeModules, taskDir)
+      // Install the task's dependencies
+      cd(path)
+      exec('yarn -s')
+      // Import the task
+      let runner = (await import(path))
+      runner = (runner.default ? runner.default : runner) as ITaskRunner
+      // Move back up to the root directory
+      cd(rootDir)
+      // Start the runner
+      cli.action.start(item.name.toLowerCase(), undefined, { stdout: true })
+      await runner({ version: item.task.version, command: 'install' })
+      cli.action.stop()
+    })
+  }
+}
