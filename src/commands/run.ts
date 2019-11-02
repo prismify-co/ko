28625c8745dc246ed00cb439cf532ec0568ecf02
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-01 22:41:48 -0600

    Add debug and spinner

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,11 +1,19 @@
 import { Command } from '@oclif/command'
-// import * as inquirer from 'inquirer'
+import { cli } from 'cli-ux'
 
 import run from '../actions/run'
+import { load } from '../utils/config'
+// import * as inquirer from 'inquirer'
 
 export class CreateCommand extends Command {
   static description = 'start the configuration process'
   async run() {
-    run()
+    try {
+      cli.action.start(`configuring your ${load().framework.name} app`)
+      await run()
+      cli.action.stop()
+    } catch (error) {
+      this.catch(error)
+    }
   }
 }
