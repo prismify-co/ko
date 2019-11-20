c76bec17d76189d17169bdd104e5bb3ca565afe2
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-20 12:33:38 -0700

    Fix exec

--- src/utils/task-manager.ts
+++ src/utils/task-manager.ts
@@ -1,18 +1,21 @@
+import * as child_process from 'child_process'
 import { cli } from 'cli-ux'
 import { join } from 'path'
-import { cd, exec } from 'shelljs'
+import { promisify } from 'util'
 
 import { IConfig } from '../interfaces/config'
 import { ITaskRunner } from '../interfaces/task-runner'
+const exec = promisify(child_process.exec)
 
 export function manage(config: IConfig) {
+  // Save the current root directory
+  const rootDir = process.cwd()
   if (Array.isArray(config.tasks)) {
     config.tasks.forEach(async item => {
       if (item.task.url !== config.repository.url) {
         // TODO: Handle tasks that have a different url
       }
-      // Save the current root directory
-      const rootDir = process.cwd()
+
       // Create the path to node_modules
       const nodeModules = join(rootDir, 'node_modules')
       // Create the path to the task's directory
@@ -20,13 +23,13 @@ export function manage(config: IConfig) {
       // Create the full path
       const path = join(nodeModules, taskDir)
       // Install the task's dependencies
-      cd(path)
-      exec('yarn -s')
+      process.chdir(path)
+      await exec('yarn --force -s')
       // Import the task
       let runner = (await import(path))
-      runner = (runner.default ? runner.default : runner) as ITaskRunner
+      runner = await (runner.default ? runner.default : runner) as ITaskRunner
       // Move back up to the root directory
-      cd(rootDir)
+      process.chdir(path)
       // Start the runner
       cli.action.start(item.name.toLowerCase(), undefined, { stdout: true })
       await runner({ version: item.task.version, command: 'install' })
