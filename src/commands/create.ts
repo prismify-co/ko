2f59976301ac654e3eb5dada1f3c1ab14b839a6b
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-20 12:49:07 -0700

    Fix commands

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -3,23 +3,34 @@ import cli from 'cli-ux'
 import dbg = require('debug')
 import * as inquirer from 'inquirer'
 
-import create from '../../actions/create'
+import create from '../actions/create'
 const debug = dbg('ko:cli:create')
 
-export class CreateNuxtCommand extends Command {
+export class CreateCommand extends Command {
   static description = 'create a new project'
   static args = [
     { name: 'name', description: 'name of the project' }
   ]
   static flags = {
+    framework: flags.string({ options: ['nuxt', 'sapper', 'next'], char: 'f' }),
     version: flags.string({ char: 'v', default: 'latest' })
   }
 
   async run() {
-    const { args, flags } = this.parse(CreateNuxtCommand)
+    const { args, flags } = this.parse(CreateCommand)
 
     // Project name
     const name = (args.name || 'app') as string
+
+    // Framework
+    const framework: string = (
+      flags.framework || ((await inquirer.prompt([{
+        name: 'framework',
+        message: 'select a framework',
+        type: 'list',
+        choices: [{ name: 'Nuxt' }, { name: 'Sapper' }, { name: 'Next' }]
+      }])).framework as string).toLowerCase()
+    )
 
     // Version
     const version: string = (
@@ -35,8 +46,8 @@ export class CreateNuxtCommand extends Command {
 
     // Create the project
     try {
-      cli.action.start(`creating project: ${name}, framework: nuxt, version: ${version}`, undefined, { stdout: true })
-      await create(name, 'nuxt', version)
+      cli.action.start(`creating project: ${name}, framework: ${framework}, version: ${version}`, undefined, { stdout: true })
+      await create(name, framework, version)
       cli.action.stop()
     } catch (error) {
       debug(`ko [error]: ${error}`)
