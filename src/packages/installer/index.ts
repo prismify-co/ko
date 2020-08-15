dc1f239159df4d0eb29ee98a69137c3220e71fd1
Takeshi Iwana <tak@prismify.co>
2020-08-15 13:39:24 -0600

    Update logic for installer

--- src/packages/installer/index.ts
+++ src/packages/installer/index.ts
@@ -1,4 +1,4 @@
-import { exists } from 'fs'
+import { exists as has, readFile } from 'fs'
 import { extract, fetch as gitly } from 'gitly'
 import { homedir, tmpdir } from 'os'
 import { join, resolve } from 'path'
@@ -7,8 +7,12 @@ import { InstallContext } from '../../types'
 import { promisify } from 'util'
 
 import { Executor } from './executor'
+import pkgm from '../package-manager'
+import execa from 'execa'
 
-const existsAsync = promisify(exists)
+const exists = promisify(has)
+const read = async (path: string) =>
+  (await promisify(readFile)(path)).toString('utf-8')
 
 const isNavtiveRecipe = (path: string) => /^([\w\-_]*)$/.test(path)
 
@@ -22,7 +26,7 @@ const isLocalPath = async (path: string) =>
   !isNavtiveRecipe(path) &&
   !isUrlRecipe(path) &&
   !isRepoShorthandRecipe(path) &&
-  existsAsync(resolve(path))
+  exists(resolve(path))
 
 export default async function install({
   name,
@@ -44,13 +48,15 @@ export default async function install({
     const source = await gitly('prismify-co/ko-recipes', gitlyOpts)
     // Extract the recipes into temp dir
     const destination = await extract(source, tmpdir())
+    const path = join(destination, name, 'next')
     // Execute from directory
-    return execute(join(destination, name, 'next'), dryRun)
+    return execute(path, await entry(path, true), dryRun)
   }
 
   if (isLocalPath(name)) {
+    const path = resolve(name)
     // Execute from the local path
-    return execute(name, dryRun)
+    return execute(path, await entry(path), dryRun)
   }
 
   if (isUrlRecipe(name)) {
@@ -59,11 +65,58 @@ export default async function install({
     // Extract the recipes into temp dir
     const destination = await extract(source, tempdir())
     // Execute from the directory
-    return execute(destination, dryRun)
+    return execute(destination, await entry(destination), dryRun)
   }
 }
 
-async function execute(path: string, dryRun: boolean) {
-  const executor = (await import(path)).default as Executor
-  return executor.setOptions({ dryRun }).run()
+async function entry(
+  path: string,
+  official: boolean = false,
+  framework: string = 'next'
+) {
+  // Check if this is the official repository
+  path = official
+    ? join(path, framework, 'package.json')
+    : join(path, 'package.json')
+
+  // Determine whether the entry point exists
+  if (await exists(path)) {
+    const json = JSON.parse(await read(path))
+
+    if (!json.main) {
+      throw new Error('A valid package.json does not exist')
+    }
+
+    return resolve(path, json.main)
+  }
+
+  throw new Error('A valid package.json does not exist')
+}
+
+async function execute(path: string, entry: string, dryRun: boolean = false) {
+  // Save the current working directory
+  const cwd = process.cwd()
+  // Set the current working directory to the destination
+  process.chdir(path)
+  // Install the packages
+  await pkgm().install()
+  // Determine if we need to compile the files
+  if (await exists(resolve('tsconfig.json'))) {
+    // Install typescript (just in case)
+    await pkgm().add(['typescript'])
+    // Compile the files
+    await execa(await pkgm().which(), ['run', 'tsc'])
+  }
+  // Grab the executor
+  const executor = (await import(entry)).default as Executor
+  // Run the executor
+  executor
+    .setOptions({
+      dryRun,
+    })
+    .run()
+  // Restore the current working directory
+  process.chdir(cwd)
+
+  return { executor, destination: path, cwd }
 }
