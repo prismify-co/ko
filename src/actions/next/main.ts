c9b246c67e74f647c9e74542411bd142fac75cd7
Takeshi Iwana <tak@prismify.co>
2020-08-14 21:03:19 -0600

    Rewrite next creator

--- src/actions/next/main.ts
+++ src/actions/next/main.ts
@@ -1,29 +1,48 @@
-import { html, stripIndents } from 'common-tags'
-import dbg = require('debug')
-import { writeFileSync } from 'fs'
-import { join } from 'path'
-import { cd, mkdir } from 'shelljs'
+import dbg from 'debug'
+import { readFile, writeFile } from 'fs'
+import handlebars from 'handlebars'
+import { join, resolve } from 'path'
+import { cd, mkdir, touch } from 'shelljs'
+import git from 'simple-git'
 import { promisify } from 'util'
+import config, { Framework } from '../../utils/config'
+import pkgm from '../../packages/package-manager'
 
-import { Package } from '../../modules/main'
-import * as config from '../../utils/config'
+const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
+
+const write = async (path: string, data: string) =>
+  writeFileAsync(path, data, 'utf-8')
+const read = async (path: string) =>
+  (await readFileAsync(path)).toString('utf-8')
+
+// import execa from 'execa'
+
 const GithubContent = require('github-content')
 
 const debug = dbg('ko:actions:next:create')
 
 const gc = new GithubContent({
   owner: 'github',
-  repo: 'gitignore'
+  repo: 'gitignore',
 })
 
-const download: (param: any) => Promise<{
-  path: string,
+const download: (
+  param: any
+) => Promise<{
+  path: string
   contents: Buffer
 }> = promisify(gc.file) as any
 
-export default async function create(name: string, framework: string, version: string) {
+export default async function create(
+  name: string,
+  framework: string,
+  version: string,
+  typescript: boolean
+) {
   const cwd = process.cwd()
   const root = join(cwd, name)
+  const templatesPath = join(__dirname, 'templates')
 
   // Create the project folder
   debug(`ko [info]: creating directory ${name}`)
@@ -31,58 +50,79 @@ export default async function create(name: string, framework: string, version: s
 
   // Create the package.json file
   debug('ko [info]: writing package.json')
-  writeFileSync(join(root, 'package.json'),
-    `{
-      "name": "${name}",
-      "scripts": {
-        "dev": "next",
-        "build": "next build",
-        "build:analyze": "next build --analyze",
-        "start": "next start"
-      },
-      "dependencies": {
-        "react": "^16.12.0",
-        "react-dom": "^16.12.0"
-      },
-      "license": "MIT"
-    }`, 'utf8'
+  const pkg = await read(join(__dirname, 'templates', 'package.txt'))
+  await write(
+    join(root, 'package.json'),
+    handlebars.compile(pkg)({
+      name,
+    })
   )
 
   // Create the next.config.js file
   debug('ko [info]: writing next.config.js')
-  writeFileSync(join(root, 'next.config.js'),
-    'module.exports = {}', 'utf8'
+  await write(
+    join(root, 'next.config.js'),
+    await read(join(templatesPath, 'next.config.txt'))
   )
 
   // CD into cwd/name
   debug(`ko [info]: cd ${root}`)
   cd(root)
 
+  // Initialize git
+  await git().init()
+
   // Install next
-  debug(`ko [info]: installing next@${/[0-9]+/g.test(version) ? `v${version}` : version}`)
-  Package.add([{
-    name: 'next',
-    version,
-    dev: false
-  }], { silent: true })
-
-  // Create the pages directory
-  debug('ko [info]: creating next directories')
-  mkdir('assets', 'components', 'pages', 'public')
-
-  // Create the first page
-  debug('ko [info]: creating index.jsx')
-  writeFileSync(join(root, 'pages', 'index.jsx'), stripIndents`
-    import React from 'react'
-
-    export default () => (${html`<div><h1>Made with ko</h1></div>`})
-  `,
-    'utf8'
+  debug(`ko [info]: installing next@${version}`)
+  await pkgm().add(
+    [
+      version === 'latest' || !version ? 'next' : { name: 'next', version },
+      'react',
+      'react-dom',
+      ...(typescript
+        ? [
+            { name: 'typescript', dev: true },
+            { name: '@types/node', dev: true },
+            { name: '@types/react', dev: true },
+          ]
+        : []),
+    ],
+    { silent: true, cwd: root }
   )
+
+  if (typescript) {
+    // Create a tsconfig.json for next.json to fill it out
+    debug('ko [info]: creating tsconfig.json')
+    touch('tsconfig.json')
+  }
+
+  // Create the directories
+  debug('ko [info]: creating next directories')
+  mkdir('assets', 'components', 'pages', 'public', 'styles')
+
+  // Create the initial pages
+  debug('ko [info]: creating files under pages/')
+  const pages = ['_app.txt', '_document.txt', 'index.txt']
+
+  pages.forEach(async (file) => {
+    const script = file.replace('txt', typescript ? 'tsx' : 'js')
+    const page = await read(join(templatesPath, `pages/${file}`))
+    await write(resolve(`pages/${script}`), handlebars.compile(page)({ name }))
+  })
+
+  // Create the initial styles
+  debug('ko [info]: creating files under styles/')
+  const styles = ['globals.css', 'home.module.css']
+  styles.forEach(async (file) => {
+    await write(
+      resolve(`styles/${file}`),
+      await read(join(templatesPath, `styles/${file}`))
+    )
+  })
 
   // Write the configuration file
   debug('ko [info]: initializing configuration file')
-  config.init({ name, framework: { name: framework, version } })
+  config({ name, framework: { name: framework as Framework, version } }).init()
 
   // Download the latest gitignore for node
   debug('ko [info]: downloading .gitignore for node')
@@ -90,5 +130,9 @@ export default async function create(name: string, framework: string, version: s
 
   // Write the .gitignore file
   debug(`ko [info]: writing .gitignore to ${root}`)
-  writeFileSync(join(root, '.gitignore'), gitignore.contents.toString(), 'utf8')
+  await write(resolve('.gitignore'), gitignore.contents.toString('utf-8'))
+
+  debug(`ko [info]: Add changes to git`)
+  // Add the changes to the commit
+  await git().commit('Add initial files')
 }
