dec71a1926f2593f7f94fb1ad8bbd34310fea41c
Takeshi Iwana <tak@prismify.co>
2020-08-14 21:04:45 -0600

    Update create command

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,57 +1,88 @@
-import { Command, flags } from '@oclif/command'
+import Command, { flags } from '@oclif/command'
 import cli from 'cli-ux'
 import dbg = require('debug')
+import execa = require('execa')
 import * as inquirer from 'inquirer'
+import { merge, omit } from 'lodash'
 
 import create from '../actions/create'
+import { CreateContext } from '../types'
 const debug = dbg('ko:cli:create')
 
 export class CreateCommand extends Command {
   static description = 'create a new project'
-  static args = [
-    { name: 'name', description: 'name of the project' }
-  ]
+  static args = [{ name: 'name', description: 'name of the project' }]
   static flags = {
-    framework: flags.string({ options: ['nuxt', 'sapper', 'next'], char: 'f' }),
-    version: flags.string({ char: 'v', default: 'latest' })
+    framework: flags.string({ options: ['next'], default: 'next', char: 'f' }),
+    version: flags.string({ char: 'v', default: 'latest' }),
+    typescript: flags.boolean({ default: true, char: 't' }),
+    prompt: flags.boolean({ default: false, char: 'p' }),
   }
 
   async run() {
     const { args, flags } = this.parse(CreateCommand)
 
-    // Project name
+    // Set the project name
     const name = (args.name || 'app') as string
+    // Set the initial context for project creation
+    let context: CreateContext = { name, ...omit(flags, 'prompt') }
+    // Update the context if prompt was specified
+    if (flags.prompt) context = merge(context, await prompt())
 
-    // Framework
-    const framework: string = (
-      flags.framework || ((await inquirer.prompt([{
-        name: 'framework',
-        message: 'select a framework',
-        type: 'list',
-        choices: [{ name: 'Nuxt' }, { name: 'Next' }, { name: 'Sapper' }]
-      }])).framework as string).toLowerCase()
-    )
-
-    // Version
-    const version: string = (
-      flags.version || ((await inquirer.prompt([
-        {
-          name: 'version',
-          message: 'set version for framework',
-          type: 'input'
-        }
-      ])
-      ).version as string).replace(/v/, '')
-    )
-
-    // Create the project
     try {
-      cli.action.start(`creating project: ${name}, framework: ${framework}, version: ${version}`, undefined, { stdout: true })
-      await create(name, framework, version)
+      cli.action.start(
+        `creating project: ${name}, framework: ${context.framework}, version: ${context.version}`,
+        undefined,
+        { stdout: true }
+      )
+
+      // Create the project
+      await create(context)
+
       cli.action.stop()
     } catch (error) {
       debug(`ko [error]: ${error}`)
       this.catch(error)
     }
   }
+}
+
+async function prompt() {
+  const framework = ((
+    await inquirer.prompt([
+      {
+        name: 'framework',
+        message: 'select a framework',
+        type: 'list',
+        choices: [{ name: 'Next' }],
+      },
+    ])
+  ).framework as string).toLowerCase()
+
+  const versions = (await execa('npm', ['view', 'next', 'versions'])).stdout
+    .replace(/\[|\]/g, '')
+    .split(',')
+
+  const version = ((
+    await inquirer.prompt([
+      {
+        name: 'version',
+        message: 'set version for framework',
+        type: 'list',
+        choices: versions.slice(-5),
+      },
+    ])
+  ).version as string).replace(/v/, '')
+
+  const typescript =
+    (await inquirer.prompt([
+      {
+        name: 'typescript',
+        message: '',
+        type: 'input',
+        choices: ['true', 'false'],
+        default: 'true',
+      },
+    ])) === 'true'
+  return { framework, version, typescript }
 }
