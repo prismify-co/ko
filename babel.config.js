d1d95891d9e6a5dfdb7af243f5b384ba4bdaf5fb
Takeshi Iwana <tak@prismify.co>
2020-08-16 17:40:27 -0600

    Fix build

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     [
       'module-resolver',
       {
-        root: ['./'],
+        root: ['./src'],
         alias: {
           '@ko/utils/(.+)': './utils/\\1',
           '@ko': ([, name]) => {
