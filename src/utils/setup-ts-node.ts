7e64bc4f9b0f560c5469ee4893fb7b56af68ab77
Takeshi Iwana <tak@prismify.co>
2020-08-16 00:55:58 -0600

    Disable tsconfig-paths

--- src/utils/setup-ts-node.ts
+++ src/utils/setup-ts-node.ts
@@ -1,9 +1,15 @@
 import { REGISTER_INSTANCE } from 'ts-node'
+import dbg from 'debug'
+const debug = dbg('ko:utils:setup-ts-node')
 
 export const setupTsnode = () => {
-  if (!process[REGISTER_INSTANCE]) {
-    // During blitz interal dev, oclif automaticaly sets up ts-node so we have to check
-    require('ts-node').register({ compilerOptions: { module: 'commonjs' } })
+  try {
+    if (!process[REGISTER_INSTANCE]) {
+      require('ts-node').register({ compilerOptions: { module: 'commonjs' } })
+    }
+    // require('tsconfig-paths/register')
+  } catch (error) {
+    debug('An error occurred while configuring ts-node')
+    debug(error)
   }
-  require('tsconfig-paths/register')
 }
