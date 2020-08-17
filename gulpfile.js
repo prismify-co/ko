9cc4e396e2f293b74e01d183786105e8df526281
Takeshi Iwana <tak@prismify.co>
2020-08-17 10:47:31 -0600

    Use gulp to build the project

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { series, src, dest } = require('gulp')
+const babel = require('gulp-babel')
+
+function build() {
+  return src('./src/**/*.ts').pipe(babel()).pipe(dest('lib'))
+}
+
+function copy() {
+  return src(['src/**/*.txt', 'src/**/*.css']).pipe(dest('lib'))
+}
+
+exports.default = series(build, copy)
