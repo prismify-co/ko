2ae549572e73df83dbc3a74e890c305f0eab9df1
Takeshi Iwana <tak@prismify.co>
2020-08-17 22:25:23 -0600

    Fix tests

--- src/packages/builder/__test__/__fixtures__/recipe.ts
+++ src/packages/builder/__test__/__fixtures__/recipe.ts
@@ -59,7 +59,7 @@ export default builder()
   .setOwner('iwatakeshi@gmail.com')
   .addDependencyStep({
     name: 'Add npm dependencies',
-    explanation: `Chakra requires some other dependencies like emotion to work`,
+    summary: `Chakra requires some other dependencies like emotion to work`,
     packages: [
       '@chakra-ui/core',
       '@emotion/core',
@@ -69,7 +69,7 @@ export default builder()
   })
   .addTransformStep({
     name: 'Import ThemeProvider and CSSReset component',
-    explanation: `We can import the chakra provider into _app, so it is accessible to the whole app`,
+    summary: `We can import the chakra provider into _app, so it is accessible to the whole app`,
     files: [`pages/_app${extension(true)}`],
     transform(ast: ASTNode, b: builders, t: NamedTypes) {
       const stylesImport = b.importDeclaration(
--- src/packages/builder/__test__/index.test.ts
+++ src/packages/builder/__test__/index.test.ts
@@ -1,49 +1,42 @@
-import {
-  existsSync as exists,
-  mkdir as createDir,
-  readFileSync as read,
-} from 'fs'
 import { join } from 'path'
-import { promisify } from 'util'
-import { rm } from 'shelljs'
-import create from '@ko/core/create/next'
+import { rm, mkdir } from 'shelljs'
 import { entry, execute } from '@ko/core/install'
 import { setupTsnode } from '@ko/utils/setup-ts-node'
-
-const mkdir = promisify(createDir)
+import generate from '@ko/core/generate'
+import { exists, read } from '@ko/utils/fs'
 
 const CWD = __dirname
-
+const APP_NAME = 'app-recipe'
 const OUTPUT_DIR = join(CWD, 'output')
-const APP_DIR = join(OUTPUT_DIR, 'app')
+const APP_DIR = join(OUTPUT_DIR, APP_NAME)
 const RECIPE_DIR = join(CWD, '__fixtures__')
 
 jest.setTimeout(10 * 100000)
 
 async function createOutputDir() {
   if (!exists(OUTPUT_DIR)) {
-    await mkdir(OUTPUT_DIR)
+    mkdir(OUTPUT_DIR)
   } else {
     rm('-rf', OUTPUT_DIR)
-    await mkdir(OUTPUT_DIR)
+    mkdir(OUTPUT_DIR)
   }
   process.chdir(OUTPUT_DIR)
-}
-
-async function removeOutputDir() {
-  process.chdir(CWD)
 }
 
 describe('packages/installer', () => {
   beforeAll(async () => {
     setupTsnode()
     await createOutputDir()
-    await create('app', 'next', '', true)
+    await generate({
+      framework: 'next',
+      name: APP_NAME,
+      typescript: true,
+    })
     process.chdir(APP_DIR)
   })
 
   afterAll(async () => {
-    await removeOutputDir()
+    process.chdir(CWD)
   })
 
   describe('entry', () => {
@@ -54,15 +47,15 @@ describe('packages/installer', () => {
 
   describe('execute', () => {
     it('should modify a Next.js App', async () => {
-      const expected = read(join(RECIPE_DIR, '_app_expected.txt'))
-        .toString('utf-8')
-        .replace(/\s/g, '')
+      const expected = read(join(RECIPE_DIR, '_app_expected.txt')).replace(
+        /\s/g,
+        ''
+      )
 
       const entryPath = await entry(RECIPE_DIR)
       const { executor } = await execute(APP_DIR, RECIPE_DIR, entryPath, false)
-      const actual = read(join(APP_DIR, 'pages', '_app.tsx'))
-        .toString('utf-8')
-        .replace(/\s/g, '')
+      const actual = read(join(APP_DIR, 'pages', '_app.tsx')).replace(/\s/g, '')
+      expect(exists(join(APP_DIR, 'pages', '_app.tsx'))).toEqual(true)
       expect(executor.commits.length).toBeGreaterThan(0)
       expect(actual).toEqual(expected)
     })
