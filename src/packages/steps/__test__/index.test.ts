481f7d876858b8cf2fb2acabb1470ad67f64998a
Takeshi Iwana <tak@prismify.co>
2020-08-18 18:47:52 -0600

    Add tests for steps

---
+++ src/packages/steps/__test__/index.test.ts
@@ -0,0 +1,58 @@
+import Steps from '..'
+
+describe('packages/steps', () => {
+  let s = new Steps()
+  beforeEach(() => {
+    s = new Steps()
+  })
+
+  it('should add a dependency step', () => {
+    const step = {
+      name: 'test',
+      packages: ['node'],
+      condition: true,
+      summary: 'Hello',
+    }
+    s.addDependencyStep(step)
+
+    expect(s.steps.length).toEqual(1)
+    expect(s.steps[0]).toEqual({ type: 'dependency', ...step })
+  })
+
+  it('should add a file step', () => {
+    const step = {
+      name: 'test',
+      path: 'path',
+      target: 'target',
+      condition: true,
+      context: { hello: 'world' },
+      summary: 'Hello',
+    }
+    s.addFileStep(step)
+    expect(s.steps.length).toEqual(1)
+    expect(s.steps[0]).toEqual({ type: 'file', ...step })
+  })
+
+  it('should add a transform step', () => {
+    const step = {
+      name: 'test',
+      files: ['file'],
+      transform(ast: any) {
+        return ast
+      },
+    }
+    s.addTransformStep(step)
+    expect(s.steps.length).toEqual(1)
+    expect(s.steps[0]).toEqual({ type: 'transform', ...step })
+  })
+
+  it('should add a custom step', () => {
+    const step = {
+      name: 'test',
+      run() {},
+    }
+    s.addCustomStep(step)
+    expect(s.steps.length).toEqual(1)
+    expect(s.steps[0]).toEqual({ type: 'custom', ...step })
+  })
+})
