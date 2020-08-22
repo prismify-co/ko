91356a0d1e237cdf98cc2d9bc455fd3fae80dfa6
Takeshi Iwana <tak@prismify.co>
2020-08-22 10:30:13 -0600

    Fix windows tests

--- src/packages/utils/tests.ts
+++ src/packages/utils/tests.ts
@@ -1,13 +1,14 @@
 import { join } from 'path'
 import { mkdir, rm } from 'shelljs'
 import tempdir from 'temp-dir'
+import { platform, tmpdir } from 'os'
 
 /**
  * Returns the test directory.
  * @param paths The path which is followed by the test directory
  */
 export const testdir = (...paths: string[]) =>
-  join(tempdir, 'ko-test', ...paths)
+  join(platform() === 'win32' ? tmpdir() : tempdir, 'ko-test', ...paths)
 /**
  * Creates the test directory.
  * @param paths The path which is followed by the test directory
