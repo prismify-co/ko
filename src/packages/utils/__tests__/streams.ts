f2e336252a3dd440d0789e8fbc0d6efdf9116ec3
Takeshi Iwana <tak@prismify.co>
2020-08-22 00:17:08 -0600

    Add streams test

---
+++ src/packages/utils/__tests__/streams.ts
@@ -0,0 +1,43 @@
+import { ReadStream } from 'fs'
+// @ts-ignore
+import StreamTest from 'streamtest'
+import { handlebars, transformer } from '../streams'
+
+describe('packages/utils/streams', () => {
+  describe('handlebars', () => {
+    it('should stream and interpolate the input', async () => {
+      const actual = await new Promise<string>((resolve, reject) => {
+        ;(StreamTest.v2.fromChunks(['{{greeting}}']) as ReadStream)
+          .pipe(handlebars({ greeting: 'hello' }))
+          .pipe(
+            StreamTest.v2.toText(function (error: Error, text: string) {
+              if (error) reject(error)
+              else resolve(text)
+            })
+          )
+      })
+      expect(actual).toEqual('hello')
+    })
+  })
+
+  describe('transformer', () => {
+    it('should stream and return an identity', async () => {
+      const actual = await new Promise<string>((resolve, reject) => {
+        ;(StreamTest.v2.fromChunks(['hello']) as ReadStream)
+          .pipe(
+            transformer(function (ast) {
+              return ast
+            })
+          )
+          .pipe(
+            StreamTest.v2.toText(function (error: Error, text: string) {
+              if (error) reject(error)
+              else resolve(text)
+            })
+          )
+      })
+
+      expect(actual).toEqual('hello')
+    })
+  })
+})
