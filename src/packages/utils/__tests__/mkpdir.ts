604f63af25db81797ffb9b10ef16569e60f77695
Takeshi Iwana <tak@prismify.co>
2020-09-19 12:05:21 -0600

    Remove mkpdir

--- src/packages/utils/__tests__/mkpdir.ts
+++
@@ -1,7 +0,0 @@
-import { mkpdir } from '../mkpdir'
-
-describe('packages/utils/mkpdir.ts', () => {
-  it('should return undefined when the directory is "."', () => {
-    expect(mkpdir('.')).toBeUndefined()
-  })
-})
